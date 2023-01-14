77b2f180a2d64218b119619a66e163c86ed3e49f
strawstack <rhshadowgreen@gmail.com>
2023-01-13 19:10:13 -0800

    add sound files and code

---
+++ main.py
@@ -0,0 +1,5 @@
+from chessSounds import make
+
+soundStr = "black_has_pawns_on a7 s b6 s and c7 s s black king on b8 s black rook on c8 s white knight on a5 s white queen on e4 s s white_to_move mate_in_one"
+
+make(soundStr.split(" "))
